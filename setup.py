93af3fb01acbb7decd7752a6f556cfba0210015b
Hugo Dupras <hugo.dupras@nxp.com>
2018-06-04 14:22:07 +0200

    Pypi preparation
    This package is now named pyatmo to avoid conflict with lnetamo
    
    Signed-off-by: Hugo Dupras <hugo.dupras@nxp.com>

--- setup.py
+++ setup.py
@@ -5,17 +5,17 @@ from distutils.core import setup
 
 
 setup(
-    name='lnetatmo',
-    version='0.9.2.1',  # Should be updated with new versions
-    author='Philippe Larduinat',
-    author_email='philippelt@users.sourceforge.net',
-    py_modules=['lnetatmo'],
+    name='pyatmo',
+    version='1.0',  # Should be updated with new versions
+    author='Hugo Dupras',
+    author_email='jabesq@gmail.com',
+    py_modules=['pyatmo'],
     packages=['smart_home'],
     package_dir={'smart_home': 'smart_home'},
     scripts=[],
     data_files=[],
-    url='https://github.com/philippelt/netatmo-api-python',
+    url='https://github.com/jabesq/netatmo-api-python',
     license='Open Source',
-    description='Simple API to access Netatmo weather station data from any python script.',
+    description='Simple API to access Netatmo weather station data from any python script. Design for Home-Assitant (but not only)',
     long_description=open('README.md').read()
 )

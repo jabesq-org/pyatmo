9c8061bc331d85360748ce1e1dd4f57fdb7772bf
cgtobi <cgtobi@gmail.com>
2021-11-20 23:13:41 +0000

    Restructuring

---
+++ src/pyatmo/base_class.py
@@ -0,0 +1,38 @@
+"""Base class for Netatmo entities."""
+from __future__ import annotations
+
+import logging
+from abc import ABC
+
+from .device_types import NetatmoDeviceType
+
+LOG = logging.getLogger(__name__)
+
+
+NETATMO_ATTRIBUTES_MAP = {
+    "entity_id": lambda x, y: x.get("id", y),
+    "modules": lambda x, y: x.get("modules_bridged", y),
+    "device_type": lambda x, y: NetatmoDeviceType(x.get("type", y)),
+    "reachable": lambda x, _: x.get("reachable", False),
+}
+
+
+def default(key, val):
+    return lambda x, _: x.get(key, val)
+
+
+class NetatmoBase(ABC):
+    """Base class for Netatmo entities."""
+
+    entity_id: str
+    name: str
+
+    def __init__(self, raw_data: dict) -> None:
+        self.entity_id = raw_data["id"]
+        self.name = raw_data.get("name", "Unknown")
+
+    def update_topology(self, raw_data: dict) -> None:
+        self.__dict__ = {
+            key: NETATMO_ATTRIBUTES_MAP.get(key, default(key, val))(raw_data, val)
+            for key, val in self.__dict__.items()
+        }
--- src/pyatmo/module.py
+++ src/pyatmo/module.py
@@ -5,6 +5,7 @@ import logging
 from dataclasses import dataclass
 from typing import TYPE_CHECKING
 
+from .base_class import NetatmoBase
 from .device_types import NetatmoDeviceType
 
 if TYPE_CHECKING:
@@ -13,51 +14,52 @@ if TYPE_CHECKING:
 LOG = logging.getLogger(__name__)
 
 
+class MixinBase:
+    def update_topology(self, raw_data: dict) -> None:
+        pass
+
+
+class BoilerMixin:
+    boiler_status: bool | None = None
+
+
+class BatteryMixin:
+    battery_state: str | None = None
+    battery_level: int | None = None
+
+
 @dataclass
-class NetatmoModule:
+class NetatmoModule(NetatmoBase):
     """Class to represent a Netatmo module."""
 
-    entity_id: str
-    name: str
     device_type: NetatmoDeviceType
     home: NetatmoHome
     room_id: str | None
 
-    reachable: bool
-    bridge: NetatmoModule | None
-    modules: list[str]
-
-    battery_state: str | None = None
-    battery_level: int | None = None
-    boiler_status: bool | None = None
+    modules: list[str] | None
+    reachable: bool = False
+    bridge: NetatmoModule | None = None
 
     def __init__(self, home: NetatmoHome, module: dict) -> None:
-        self.entity_id = module["id"]
-        self.name = module.get("name", "Unkown")
+        super().__init__(module)
         self.device_type = NetatmoDeviceType(module["type"])
         self.home = home
         self.room_id = module.get("room_id")
         self.reachable = False
         self.bridge = module.get("bridge")
-        self.modules = module.get("modules_bridged", [])
-
-    def update_topology(self, raw_data: dict) -> None:
-        self.name = raw_data.get("name", "Unkown")
-        self.device_type = NetatmoDeviceType(raw_data["type"])
-        self.room_id = raw_data.get("room_id")
-        self.bridge = raw_data.get("bridge")
-        self.modules = raw_data.get("modules_bridged", [])
+        self.modules = module.get("modules_bridged")
 
     def update(self, raw_data: dict) -> None:
-        self.reachable = raw_data.get("reachable", False)
-        self.boiler_status = raw_data.get("boiler_status")
-        self.battery_level = raw_data.get("battery_level")
-        self.battery_state = raw_data.get("battery_state")
+        self.update_topology(raw_data)
 
-        if not self.reachable:
+        if not self.reachable and self.modules:
             # Update bridged modules and associated rooms
             for module_id in self.modules:
                 module = self.home.modules[module_id]
                 module.update(raw_data)
                 if module.room_id:
                     self.home.rooms[module.room_id].update(raw_data)
+
+
+class NVAModule(BatteryMixin, BoilerMixin, NetatmoModule):
+    pass

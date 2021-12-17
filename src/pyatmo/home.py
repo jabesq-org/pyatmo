7744c4c27cef3d4407ac6a796c81df42036c2ab5
cgtobi <cgtobi@gmail.com>
2021-12-17 21:22:44 +0000

    Refactor
    Signed-off-by: cgtobi <cgtobi@gmail.com>

--- src/pyatmo/home.py
+++ src/pyatmo/home.py
@@ -1,13 +1,8 @@
 """Module to represent a Netatmo home."""
 from __future__ import annotations
 
-from abc import ABC
 from dataclasses import dataclass
-from typing import Callable
 
-from pyatmo.auth import AbstractAsyncAuth
-from pyatmo.const import _GETHOMESDATA_REQ
-from pyatmo.helpers import extract_raw_data_new
 from pyatmo.modules import netatmo as netatmo_modules
 from pyatmo.room import NetatmoRoom
 from pyatmo.schedule import NetatmoSchedule
@@ -117,58 +112,3 @@ class NetatmoHome:
         if (schedule := self.get_selected_schedule()) is None:
             return None
         return schedule.away_temp
-
-
-class AbstractClimateTopology(ABC):
-    """Abstract class of Netatmo energy device topology."""
-
-    home_ids: list[str] = []
-    subscriptions: dict[str, Callable] = {}
-    raw_data: dict = {}
-
-    def __repr__(self) -> str:
-        return f"{self.__class__.__name__}(home_ids={self.home_ids}, subscriptions={self.subscriptions})"
-
-    def register_handler(self, home_id: str, handler: Callable) -> None:
-        """Register update handler."""
-        if self.subscriptions.get(home_id) == handler:
-            return
-
-        if home_id in self.subscriptions and self.subscriptions[home_id] != handler:
-            self.unregister_handler(home_id)
-
-        self.subscriptions[home_id] = handler
-
-        self.publish()
-
-    def unregister_handler(self, home_id: str) -> None:
-        """Unregister update handler."""
-        self.subscriptions.pop(home_id)
-
-    def publish(self) -> None:
-        """Publish latest data to subscribers."""
-        for home in self.raw_data.get("homes", []):
-            if (home_id := home["id"]) in self.subscriptions:
-                self.subscriptions[home_id](home)
-
-
-class AsyncClimateTopology(AbstractClimateTopology):
-    """Async class of Netatmo energy device topology."""
-
-    def __init__(self, auth: AbstractAsyncAuth) -> None:
-        """Initialize the Netatmo home data.
-
-        Arguments:
-            auth {AbstractAsyncAuth} -- Authentication information with a valid access token
-        """
-        self.auth = auth
-
-    async def async_update(self) -> None:
-        """Retrieve status updates from /homesdata."""
-        resp = await self.auth.async_post_request(url=_GETHOMESDATA_REQ)
-        self.raw_data = extract_raw_data_new(await resp.json(), "homes")
-
-        for home in self.raw_data["homes"]:
-            self.home_ids.append(home["id"])
-
-        self.publish()

7bbb0f6f606fff151427cd771a14808337878a1f
cgtobi <cgtobi@gmail.com>
2021-12-18 11:04:00 +0000

    Prepare weather sensors
    Signed-off-by: cgtobi <cgtobi@gmail.com>

--- src/pyatmo/modules/__init__.py
+++ src/pyatmo/modules/__init__.py
@@ -1,6 +1,21 @@
 """Expose submodules."""
 from .idiamant import NBG, NBR
-from .netatmo import NDB, NOC, NRV, OTH, OTM, NACamera, NAPlug, NATherm1, NetatmoModule
+from .netatmo import (
+    NDB,
+    NOC,
+    NRV,
+    OTH,
+    OTM,
+    NACamera,
+    NAMain,
+    NAModule1,
+    NAModule2,
+    NAModule3,
+    NAModule4,
+    NAPlug,
+    NATherm1,
+    NetatmoModule,
+)
 
 __all__ = [
     "NetatmoModule",
@@ -14,4 +29,9 @@ __all__ = [
     "NATherm1",
     "OTH",
     "OTM",
+    "NAMain",
+    "NAModule1",
+    "NAModule2",
+    "NAModule3",
+    "NAModule4",
 ]

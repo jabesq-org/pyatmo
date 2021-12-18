94a777055a11b911b06d8b886194eb815dca55ff
cgtobi <cgtobi@gmail.com>
2021-12-18 10:50:24 +0000

    Add idiamant
    Signed-off-by: cgtobi <cgtobi@gmail.com>

---
+++ src/pyatmo/modules/idiamant.py
@@ -0,0 +1,16 @@
+"""Module to represent iDiamant modules."""
+from __future__ import annotations
+
+import logging
+
+from pyatmo.modules.module import NetatmoModule
+
+LOG = logging.getLogger(__name__)
+
+
+class NBG(NetatmoModule):
+    pass
+
+
+class NBR(NetatmoModule):
+    pass

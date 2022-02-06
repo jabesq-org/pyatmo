e9abf5a75c053fd1c32bdd886d01f820170f18b6
Sourcery AI <>
2022-02-06 01:08:34 +0000

    'Refactored by Sourcery'

--- tests/conftest.py
+++ tests/conftest.py
@@ -131,15 +131,16 @@ def camera_ping(requests_mock):
         with open("fixtures/camera_ping.json", encoding="utf-8") as json_file:
             json_fixture = json.load(json_file)
         requests_mock.post(
-            vpn_url + "/command/ping",
+            f'{vpn_url}/command/ping',
             json=json_fixture,
             headers={"content-type": "application/json"},
         )
+
     local_url = "http://192.168.0.123/678460a0d47e5618699fb31169e2b47d"
     with open("fixtures/camera_ping.json", encoding="utf-8") as json_file:
         json_fixture = json.load(json_file)
     requests_mock.post(
-        local_url + "/command/ping",
+        f'{local_url}/command/ping',
         json=json_fixture,
         headers={"content-type": "application/json"},
     )

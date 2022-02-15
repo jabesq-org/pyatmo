705e8864ee1f592afbef6ef08ae9f6ba2bdb1250
cgtobi <cgtobi@gmail.com>
2022-02-15 20:01:23 +0000

    Add device description mapping
    Signed-off-by: cgtobi <cgtobi@gmail.com>

--- src/pyatmo/modules/device_types.py
+++ src/pyatmo/modules/device_types.py
@@ -1,4 +1,4 @@
-"""Deifinitions of Netatmo devices types."""
+"""Definitions of Netatmo devices types."""
 from __future__ import annotations
 
 import logging
@@ -9,7 +9,7 @@ LOG = logging.getLogger(__name__)
 # pylint: disable=W0613,R0201
 
 
-class NetatmoDeviceType(Enum):
+class NetatmoDeviceType(str, Enum):
     """Class to represent Netatmo device types."""
 
     # temporarily disable locally-disabled and locally-enabled
@@ -29,7 +29,7 @@ class NetatmoDeviceType(Enum):
     NIS = "NIS"  # Smart Indoor Siren
     NACamDoorTag = "NACamDoorTag"  # Smart Door and Window Sensors
     NDB = "NDB"  # Smart Video Doorbell
-    NCO = "NCO"  # Smark Carbon Monoxide Alarm
+    NCO = "NCO"  # Smart Carbon Monoxide Alarm
 
     # Weather
     NAMain = "NAMain"  # Smart Home Weather Station
@@ -37,6 +37,7 @@ class NetatmoDeviceType(Enum):
     NAModule2 = "NAModule2"
     NAModule3 = "NAModule3"
     NAModule4 = "NAModule4"
+    public = "public"
 
     # Home Coach
     NHC = "NHC"  # Smart Indoor Air Quality Monitor
@@ -77,3 +78,86 @@ class NetatmoDeviceType(Enum):
     NBS = "NBS"  # swing shutter
 
     # pylint: enable=C0103
+
+
+class NetatmoDeviceCategory(str, Enum):
+    """Class to represent Netatmo device types."""
+
+    # temporarily disable locally-disabled and locally-enabled
+    # pylint: disable=C0103
+
+    climate = "climate"
+    camera = "camera"
+    siren = "siren"
+    shutter = "shutter"
+    lock = "lock"
+    plug = "plug"
+    sensor = "sensor"
+
+    # pylint: enable=C0103
+
+
+DEVICE_CATEGORY_MAP: dict[NetatmoDeviceType, NetatmoDeviceCategory] = {
+    NetatmoDeviceType.NRV: NetatmoDeviceCategory.climate,
+    NetatmoDeviceType.NATherm1: NetatmoDeviceCategory.climate,
+    NetatmoDeviceType.OTM: NetatmoDeviceCategory.climate,
+}
+
+
+DEVICE_DESCRIPTION_MAP: dict[NetatmoDeviceType, str] = {
+    # Climate/Energy
+    NetatmoDeviceType.NAPlug: "Smart Thermostat Gateway",
+    NetatmoDeviceType.NATherm1: "Smart Thermostat",
+    NetatmoDeviceType.NRV: "Smart Valve",
+    NetatmoDeviceType.OTH: "OpenTherm Gateway",
+    NetatmoDeviceType.OTM: "OpenTherm Modulating Thermostat",
+    # Cameras/Security,
+    NetatmoDeviceType.NOC: "Smart Outdoor Camera",
+    NetatmoDeviceType.NACamera: "Smart Indoor Camera",
+    NetatmoDeviceType.NSD: "Smart Smoke Detector",
+    NetatmoDeviceType.NIS: "Smart Indoor Siren",
+    NetatmoDeviceType.NACamDoorTag: "Smart Door/Window Sensors",
+    NetatmoDeviceType.NDB: "Smart Video Doorbell",
+    NetatmoDeviceType.NCO: "Smart Carbon Monoxide Alarm",
+    # Weather,
+    NetatmoDeviceType.NAMain: "Smart Home Weather station",
+    NetatmoDeviceType.NAModule1: "Smart Outdoor Module",
+    NetatmoDeviceType.NAModule2: "Smart Anemometer",
+    NetatmoDeviceType.NAModule3: "Smart Rain Gauge",
+    NetatmoDeviceType.NAModule4: "Smart Indoor Module",
+    NetatmoDeviceType.public: "Public Weather station",
+    # Home Coach,
+    NetatmoDeviceType.NHC: "Smart Indoor Air Quality Monitor",
+    # 3rd Party,
+    NetatmoDeviceType.BNS: "Smarther with Netatmo",
+    # Legrand Wiring devices and electrical panel products,
+    NetatmoDeviceType.NLG: "Gateway",
+    NetatmoDeviceType.NLGS: "Gateway standalone",
+    NetatmoDeviceType.NLP: "Plug",
+    NetatmoDeviceType.NLPM: "Mobile plug",
+    NetatmoDeviceType.NLPBS: "British standard plugs",
+    NetatmoDeviceType.NLF: "2 wire light switch",
+    NetatmoDeviceType.NLFN: "Light switch with neutral",
+    NetatmoDeviceType.NLM: "Light micro module",
+    NetatmoDeviceType.NLL: "Italian light switch with neutral",
+    NetatmoDeviceType.NLV: "Legrand/BTicino Shutters",
+    NetatmoDeviceType.NLLV: "Legrand/BTicino Shutters",
+    NetatmoDeviceType.NLLM: "Legrand/BTicino Shutters",
+    NetatmoDeviceType.NLPO: "Connected Contactor",
+    NetatmoDeviceType.NLPT: "Connected Latching Relay",
+    NetatmoDeviceType.NLPC: "Connected Energy Meter",
+    NetatmoDeviceType.NLE: "Connected Ecometer",
+    NetatmoDeviceType.NLPS: "Smart Load Shedder",
+    NetatmoDeviceType.NLC: "Cable Outlet",
+    NetatmoDeviceType.NLT: "Global Remote Control",
+    # BTicino Classe 300 EOS,
+    NetatmoDeviceType.BNCX: "Internal Panel",
+    NetatmoDeviceType.BNEU: "External Unit",
+    NetatmoDeviceType.BNDL: "Door Lock",
+    NetatmoDeviceType.BNSL: "Staircase Light",
+    # Bubbendorf shutters,
+    NetatmoDeviceType.NBG: "Gateway",
+    NetatmoDeviceType.NBR: "Roller Shutter",
+    NetatmoDeviceType.NBO: "Orientable Shutter",
+    NetatmoDeviceType.NBS: "Swing Shutter",
+}

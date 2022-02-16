f9603fc91538e930142b56e7b0cc4ba0f4cd6648
cgtobi <cgtobi@gmail.com>
2022-02-16 21:39:18 +0000

    Add room climate device type
    Signed-off-by: cgtobi <cgtobi@gmail.com>

--- src/pyatmo/__init__.py
+++ src/pyatmo/__init__.py
@@ -1,12 +1,15 @@
 """Expose submodules."""
+from pyatmo import modules
 from pyatmo.account import AsyncAccount
 from pyatmo.auth import AbstractAsyncAuth, ClientAuth, NetatmoOAuth2
 from pyatmo.camera import AsyncCameraData, CameraData
 from pyatmo.exceptions import ApiError, InvalidHome, InvalidRoom, NoDevice, NoSchedule
+from pyatmo.home import NetatmoHome
 from pyatmo.home_coach import AsyncHomeCoachData, HomeCoachData
 from pyatmo.modules import NetatmoModule
 from pyatmo.modules.device_types import NetatmoDeviceType
 from pyatmo.public_data import AsyncPublicData, PublicData
+from pyatmo.room import NetatmoRoom
 from pyatmo.thermostat import AsyncHomeData, AsyncHomeStatus, HomeData, HomeStatus
 from pyatmo.weather_station import AsyncWeatherStationData, WeatherStationData
 
@@ -27,11 +30,14 @@ __all__ = [
     "HomeStatus",
     "InvalidHome",
     "InvalidRoom",
+    "NetatmoHome",
     "NetatmoModule",
+    "NetatmoRoom",
     "NetatmoDeviceType",
     "NetatmoOAuth2",
     "NoDevice",
     "NoSchedule",
     "PublicData",
     "WeatherStationData",
+    "modules",
 ]

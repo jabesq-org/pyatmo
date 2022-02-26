19f89e8645a18604a1bf3faaacc010b236048ff0
cgtobi <cgtobi@gmail.com>
2022-02-26 14:33:41 +0000

    Import missing device types
    Signed-off-by: cgtobi <cgtobi@gmail.com>

--- src/pyatmo/modules/__init__.py
+++ src/pyatmo/modules/__init__.py
@@ -19,11 +19,13 @@ from .legrand import (
     NLV,
 )
 from .netatmo import (
+    NCO,
     NDB,
     NHC,
     NIS,
     NOC,
     NRV,
+    NSD,
     OTH,
     OTM,
     Location,
@@ -46,7 +48,8 @@ __all__ = [
     "NOC",
     "NACamDoorTag",
     "NIS",
-    "NLE",
+    "NCO",
+    "NSD",
     "NLG",
     "NLP",
     "NLT",

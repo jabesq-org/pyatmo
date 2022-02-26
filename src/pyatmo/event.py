b78e9ddd4a6cd0fed4dc93ac461439be6581429f
cgtobi <cgtobi@gmail.com>
2022-02-26 23:50:31 +0000

    Implement subevent support
    Signed-off-by: cgtobi <cgtobi@gmail.com>

--- src/pyatmo/event.py
+++ src/pyatmo/event.py
@@ -3,10 +3,6 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 from enum import Enum
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from .home import Home
 
 EVENT_ATTRIBUTES_MAP = {"id": "entity_id", "type": "event_type", "time": "event_time"}
 
@@ -94,11 +90,14 @@ class Event:
     vignette: Snapshot | None = None
     video_status: VideoStatus | None = None
     is_arrival: bool | None = None
+    subevents: list[Event] | None = None
 
-    def __init__(self, home: Home, raw_data: dict) -> None:
-        self.home_id = home.entity_id
+    def __init__(self, home_id: str, raw_data: dict) -> None:
+        self.home_id = home_id
         self._init_attributes(raw_data)
 
     def _init_attributes(self, raw_data: dict) -> None:
         for attrib, value in raw_data.items():
+            if attrib == "subevents":
+                value = [Event(self.home_id, event) for event in value]
             setattr(self, EVENT_ATTRIBUTES_MAP.get(attrib, attrib), value)

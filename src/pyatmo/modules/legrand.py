c7e7f0ea82d6299691fd09d970adf9797a9b3cf7
Tobias Sauerwein <cgtobi@gmail.com>
2022-04-27 21:49:51 +0000

    Bump pre-commit config

--- src/pyatmo/modules/legrand.py
+++ src/pyatmo/modules/legrand.py
@@ -25,94 +25,62 @@ LOG = logging.getLogger(__name__)
 class NLG(FirmwareMixin, Module):
     """Legrand gateway."""
 
-    ...
-
 
 class NLT(FirmwareMixin, Module):
     """Legrand global remote control."""
-
-    ...
 
 
 class NLP(Switch):
     """Legrand plug."""
 
-    ...
-
 
 class NLPM(Switch):
     """Legrand mobile plug."""
-
-    ...
 
 
 class NLPBS(Switch):
     """Legrand british standard plug."""
 
-    ...
-
 
 class NLF(Switch):
     """Legrand 2 wire light switch."""
-
-    ...
 
 
 class NLFN(Dimmer):
     """Legrand light switch with neutral."""
 
-    ...
-
 
 class NLM(Switch):
     """Legrand light micro module."""
-
-    ...
 
 
 class NLL(FirmwareMixin, EnergyMixin, WifiMixin, SwitchMixin, Module):
     """Legrand / BTicino italian light switch with neutral."""
 
-    ...
-
 
 class NLV(FirmwareMixin, RfMixin, ShutterMixin, Module):
     """Legrand / BTicino shutters."""
-
-    ...
 
 
 class NLLV(FirmwareMixin, RfMixin, ShutterMixin, Module):
     """Legrand / BTicino shutters."""
 
-    ...
-
 
 class NLLM(FirmwareMixin, RfMixin, ShutterMixin, Module):
     """Legrand / BTicino shutters."""
-
-    ...
 
 
 class NLPC(FirmwareMixin, HistoryMixin, PowerMixin, EnergyMixin, Module):
     """Legrand / BTicino connected energy meter."""
 
-    ...
-
 
 class NLE(FirmwareMixin, HistoryMixin, PowerMixin, EnergyMixin, Module):
     """Legrand / BTicino connected ecometer."""
-
-    ...
 
 
 class NLPS(FirmwareMixin, PowerMixin, EnergyMixin, Module):
     """Legrand / BTicino smart load shedder."""
 
-    ...
-
 
 class NLC(FirmwareMixin, SwitchMixin, Module):
     """Legrand / BTicino cable outlet."""
-
-    ...

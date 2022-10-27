373521f7d68d250ff48c9f456d0b48c1f8108c82
Sourcery AI <>
2022-10-27 06:01:40 +0000

    'Refactored by Sourcery'

--- src/pyatmo/camera.py
+++ src/pyatmo/camera.py
@@ -495,8 +495,7 @@ class CameraData(AbstractCameraData):
 
         if (vpn_url := camera_data.get("vpn_url")) and camera_data.get("is_local"):
             if temp_local_url := self._check_url(vpn_url):
-                local_url = self._check_url(temp_local_url)
-                if local_url:
+                if local_url := self._check_url(temp_local_url):
                     self.cameras[home_id][camera_id]["local_url"] = local_url
                 else:
                     LOG.warning(

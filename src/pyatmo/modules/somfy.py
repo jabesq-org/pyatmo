0f3bdc0e1e0c403067c66dd75456d6515d61fc1f
Ludovic BOUÉ <lboue@users.noreply.github.com>
2022-11-15 17:30:25 +0100

    Adding Somfy shutters

---
+++ src/pyatmo/modules/somfy.py
@@ -0,0 +1,19 @@
+"""Module to represent somfy modules."""
+from __future__ import annotations
+
+import logging
+
+from pyatmo.modules.module import (
+    FirmwareMixin,
+    Module,
+    RfMixin,
+    ShutterMixin,
+    WifiMixin,
+)
+
+LOG = logging.getLogger(__name__)
+
+class TPSRS(FirmwareMixin, RfMixin, ShutterMixin, Module):
+    ...
+	
+	

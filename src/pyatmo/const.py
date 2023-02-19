547bbb9f436a79447dc235f5dfc3607bbac82cde
Philipp Wüthrich <philipp.wuethrich@swisscom.com>
2023-02-19 16:53:02 +0000

    revert changes to thermostat

--- src/pyatmo/const.py
+++ src/pyatmo/const.py
@@ -33,7 +33,6 @@ SETROOMTHERMPOINT_ENDPOINT = "api/setroomthermpoint"
 GETROOMMEASURE_ENDPOINT = "api/getroommeasure"
 SWITCHHOMESCHEDULE_ENDPOINT = "api/switchhomeschedule"
 SYNCHOMESCHEDULE_ENDPOINT = "api/synchomeschedule"
-SYNCSCHEDULE_ENDPOINT = "api/syncschedule"
 
 GETHOMEDATA_ENDPOINT = "api/gethomedata"
 GETCAMERAPICTURE_ENDPOINT = "api/getcamerapicture"

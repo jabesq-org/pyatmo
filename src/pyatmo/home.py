ec00b304c1bf97bafa98e961a811734c8fb33dd1
Philipp Wüthrich <philipp.wuethrich@swisscom.com>
2025-04-13 19:51:14 +0000

    improve schedule validation and require an entity_id

--- src/pyatmo/home.py
+++ src/pyatmo/home.py
@@ -442,4 +442,8 @@ def is_valid_state(data: dict[str, Any]) -> bool:
 
 def is_valid_schedule(schedule: Schedule) -> bool:
     """Check schedule."""
-    return schedule is not None
+    return (
+        isinstance(schedule, Schedule)
+        and hasattr(schedule, "entity_id")
+        and schedule.entity_id != ""
+    )

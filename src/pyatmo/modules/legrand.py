e02dce83e02497a1a357985cf1826d64000fc34c
Adriano Martins <38539+adrianoapmartins@users.noreply.github.com>
2025-10-28 23:05:20 +0000

    Add NLY device class for three-phase energy meter

--- src/pyatmo/modules/legrand.py
+++ src/pyatmo/modules/legrand.py
@@ -181,3 +181,6 @@ class NLPD(OffloadMixin, Switch):
 
 class NLJ(FirmwareMixin, ShutterMixin, Module):
     """Legrand garage door opener."""
+
+class NLY(FirmwareMixin, EnergyHistoryMixin, PowerMixin, Module):
+       """Legrand connected 3 phases energy meter."""

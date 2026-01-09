e889a78571992e57f7c4e6111c7c9956a0418561
Tim Jacob <timothe.jacob@me.com>
2026-01-09 20:01:24 +0100

    fix: removed redundant normalization and applied sourcery review

--- src/pyatmo/home.py
+++ src/pyatmo/home.py
@@ -25,7 +25,6 @@ from pyatmo.exceptions import (
     InvalidStateError,
     NoScheduleError,
 )
-from pyatmo.helpers import normalize_weather_attributes
 from pyatmo.person import Person
 from pyatmo.room import Room
 from pyatmo.schedule import Schedule
@@ -175,7 +174,6 @@ class Home:
 
         has_an_update = False
         for module in data.get("modules", []):
-            module = normalize_weather_attributes(module)
             has_an_update = True
             if module["id"] not in self.modules:
                 self.update_topology({"modules": [module]})
